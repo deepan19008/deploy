5d1a9abbbfe7102805bf90eb5cb8b63055eae501
deepan19008 <104304608+deepan19008@users.noreply.github.com>
2024-07-03 18:07:20 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
     # Build your project
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p $DOCKER_PASSWORD
-    docker tag test ar8888/dev
-    docker push ar8888/dev
+    docker login -u deepanr -p dckr_pat_IOqvbW-SRoP3kEB5tvu1eHXtQNI
+    docker tag my-react-app deepanr/dev
+    docker push deepanr/dev
 
 elif [[ $GIT_BRANCH == "origin/main" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p $DOCKER_PASSWORD
-    docker tag test ar8888/prod 
-    docker push ar8888/prod
+    docker login -u deepanr -p dckr_pat_IOqvbW-SRoP3kEB5tvu1eHXtQNI
+    docker tag my-react-app deepanr/prod 
+    docker push deepanr/prod
 fi
