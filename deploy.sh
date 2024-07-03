6af804998b528dbcb8dde6c6f3bf48cd9765c20a
deepan19008 <104304608+deepan19008@users.noreply.github.com>
2024-07-03 18:37:14 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-if [[ $GIT_BRANCH == "origin/dev" ]]; then
-    # Build your project
-    sh 'chmod +x build.sh'
-    sh './build.sh'
-    docker login -u deepanr -p dckr_pat_IOqvbW-SRoP3kEB5tvu1eHXtQNI
-    docker tag my-react-app deepanr/dev
-    docker push deepanr/dev
 
-elif [[ $GIT_BRANCH == "origin/main" ]]; then
-    sh 'chmod +x build.sh'
-    sh './build.sh'
-    docker login -u deepanr -p dckr_pat_IOqvbW-SRoP3kEB5tvu1eHXtQNI
-    docker tag my-react-app deepanr/prod 
-    docker push deepanr/prod
+# Set the image name
+IMAGE_NAME="my-react-app"
+
+
+# Deploy the Docker image using Docker Compose
+echo "Deploying the Docker image..."
+ docker compose up -d
+
+# Check if the deployment was successful
+if [ $? -eq 0 ]; then
+  echo "Docker image deployed successfully."
+else
+  echo "Failed to deploy the Docker image."
+  exit 1
 fi
